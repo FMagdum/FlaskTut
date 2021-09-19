69b18ee3613426429bb7abd2b0c7c7223f125263
Faisalian <faisalmagdum5@gmail.com>
2021-09-19 19:52:10 +0530

    Tut 6 90%

--- FMagdum/routes.py
+++ FMagdum/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template,url_for, flash, redirect
-from FMagdum import app
+from FMagdum import app, db, bcrypt
 from FMagdum.forms import RegistrationForm, LoginForm
 from FMagdum.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -29,19 +30,40 @@ def about():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','Success')
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    else:
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode()
+            user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash('Your account has been created! You are now able to log in','Success')
+            return redirect(url_for('login'))
+        return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data =='password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else: 
-            flash('Login Unsuccessful. Please check username password','danger')
-    return render_template('login.html', title='Login', form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    else:
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('home'))
+            else:
+                flash('Login Unsuccessful. Please check email and password','danger')
+        return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout", methods=['GET','POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
